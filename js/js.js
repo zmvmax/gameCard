90399ae65b9744de7d8f6383886ccb734903e789
zmvmax <zmvmax@yandex.ru>
2022-01-31 23:13:03 +0200

    Изменения в js

--- js/js.js
+++ js/js.js
@@ -1,2 +1,9 @@
 'use strict';
 
+let $ = elem => document.querySelector(elem);
+let Smanual = $('#manual');
+let BgetSettingsButton = $('#BgetSettingsButton');
+let Sssettings = $('#Ssettings');
+let Sgame = $('#Sgame');
+
+BgetSettingsButton.addEventListener()

c775c3ba3b1ac3c0125701eac63b9a570eabf770
zmvmax <zmvmax@yandex.ru>
2022-01-31 23:13:03 +0200

    Изменения в js

--- js/js.js
+++ js/js.js
@@ -1,2 +1,26 @@
 'use strict';
 
+let settings = {
+    $(elem) {
+        document.querySelector(elem)
+        },
+    Smanual: $('#manual'),
+    BgetSettingsButton: $('#getSettingsButton'),
+    Ssettings: $('#settings'),
+
+    Sgame: $('#game'),
+
+}
+let getSettings = {
+    settings,
+    startValues(){
+        this.settings.BgetSettingsButton.addEventListener('click', getStartValues);
+    },
+    getStartValues(){
+      this.settings.Smanual.classList.add('off');
+    },
+}
+let run = {
+    getSettings,
+}
+run.getSettings.startValues();

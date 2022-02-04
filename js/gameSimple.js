3f6339d7413fd7120b74ae78cbf4cae5a2e8bfd7
zmvmax <zmvmax@yandex.ru>
2022-02-04 20:58:00 +0200

    JS создание heder 1

--- js/gameSimple.js
+++ js/gameSimple.js
@@ -1,7 +1,34 @@
 'use strict';
 
+let timerFunction;
+let i=10;
+
 startGame.addEventListener('click', newGame);
 function newGame(event) {
     event.preventDefault();
     startGame.classList.add('off');
+    if(viewTimerBack.textContent==='До конца игры осталось: 30 сек.') {
+        timerFunction=setInterval(gameSimpleStart, 1000);
+    }
+    creatGameField();
+    run();
 }
+function gameSimpleStart(){
+        viewTimerBack.textContent=`До конца игры осталось: ${--i} сек.`;
+        if(i===0){
+            viewTimerBack.textContent='Вы проиграли';
+            clearInterval(timerFunction);
+        }
+    }
+function run() {
+
+}
+
+function creatGameField() {
+    if(viewSizeFields.textContent==='Количество картинок: 16'){
+
+    }
+}
+
+
+
